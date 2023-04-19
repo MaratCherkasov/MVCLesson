ad2992c7f66c303f0cdbe9a96f05196a0e88a80a
Marat Cherkasov <marat.cherkasov@yandex.ru>
2023-04-19 20:21:17 +0500

    initial commit

---
+++ MVCLesson/MVCLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCLesson
+//
+//  Created by Marat Cherkasov on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
